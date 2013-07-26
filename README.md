0bc6e99cbc0347bc71f8c197db6df0a79525ffa4
deoxxa <deoxxa@fknsrs.biz>
2013-07-26 15:17:34 +1000

    code import

---
+++ README.md
@@ -0,0 +1,37 @@
+connect-leg
+===========
+
+Simple really, just a middleware for logging with leg. Or any compatible logging
+thing.
+
+Overview
+--------
+
+This middleware provides a simple logging routine designed to work with
+[leg](https://github.com/deoxxa/leg/). It works with
+[express](http://expressjs.com/) and vanilla
+[connect](http://www.senchalabs.org/connect/).
+
+Installation
+------------
+
+> npm install connect-leg
+
+Usage
+-----
+
+```js
+app.use(require("connect-leg")(log));
+```
+
+License
+-------
+
+3-clause BSD. A copy is included with the source.
+
+Contact Me
+----------
+
+* GitHub ([deoxxa](http://github.com/deoxxa))
+* Twitter ([@deoxxa](http://twitter.com/deoxxa))
+* Email ([deoxxa@fknsrs.biz](mailto:deoxxa@fknsrs.biz))
---
+++ index.js
@@ -0,0 +1,37 @@
+var connectLeg = module.exports = function connectLeg(log) {
+  return function connectLeg(req, res, next) {
+    req._leg_requestId = [Date.now(), crypto.randomBytes(4).toString("hex")].join("-");
+    req._leg_requestTime = Date.now();
+
+    log.info("http request", {
+      request: req._leg_requestId,
+      method: req.method,
+      path: req.url,
+    });
+
+    var _end = res.end;
+    res.end = function() {
+      var level;
+
+      if (res.statusCode >= 400 && res.statusCode <= 499) {
+        level = "warn";
+      } else if (res.statusCode >= 500) {
+        level = "error";
+      } else {
+        level = "info";
+      }
+
+      log[level]("response", {
+        request: req._leg_requestId,
+        method: req.method,
+        path: req.url,
+        status: res.statusCode,
+        took: Date.now() - req._leg_requestTime,
+      });
+
+      return _end.apply(res, arguments);
+    };
+
+    return next();
+  };
+};
